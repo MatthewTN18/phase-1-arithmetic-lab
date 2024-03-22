17c1ec4aad2cf8d5954c59a889adf890543cdb9d
Matthew <matthewtndirangu@gmail.com>
2024-03-22 16:15:26 +0300

    feat: complete lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+let random;
+random = Math.floor(Math.random() * 10)
+
+
+let num3 = 16
+let num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(1, 4, 7, 9, 15, 20)
+
+
+
+
+
